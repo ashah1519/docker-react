9a196c8eb11e48c7ebb52973a5afdfa17a730695
ashah_2020 <ankit.shah@nuance.com>
2021-01-14 11:52:03 -0500

    updated text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello World Docker rocks sure..HMM!!!
+          Edit <code>src/App.js</code> and save to reload. Hello World Docker rocks sure..HMM!!! New feature coming SOON !
         </p>
         <a
           className="App-link"
